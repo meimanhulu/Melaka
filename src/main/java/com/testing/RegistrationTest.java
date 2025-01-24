b4017fc6f55ad9ff7b2b9c17fc2edd3fc493da34
meimanhulu <94421861+meimanhulu@users.noreply.github.com>
2025-01-24 14:56:02 +0700

    push

--- src/main/java/com/testing/RegistrationTest.java
+++ src/main/java/com/testing/RegistrationTest.java
@@ -14,8 +14,8 @@ public class RegistrationTest {
         System.out.println("Selenium 4");
         System.setProperty("webdriver.chrome.driver", "C:\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
         driver.manage().window().maximize();
         driver.get("https://dashboard.melaka.app/register");
         
@@ -32,85 +32,93 @@ public class RegistrationTest {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
         try {
             WebElement nameField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"input-:R36l6:\"]")));
-            nameField.sendKeys("Sas"); 
+            nameField.sendKeys("Saskety"); 
             WebElement phoneField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"input-:R156l6:\"]")));
-            phoneField.sendKeys("81482723489000"); 
+            phoneField.sendKeys("81481323412"); 
             WebElement businessNameField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"input-:R76l6:\"]")));
-            businessNameField.sendKeys(""); 
+            String businessName = "Indoapril";  // Simulating input for business name
+            
+            if (businessName.isEmpty()) {
+                WebElement businessNameErrorMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"__next\"]/div/div[2]/div[2]/div/form/div[3]/div[2]/p")));
+                System.out.println("Error: " + businessNameErrorMessage.getText());
+                return;  // Stop execution if business name is empty
+            }
+            businessNameField.sendKeys(businessName); 
+
             WebElement radioButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"retail_store-:Ra96l6:\"]")));
             radioButton.click();
             WebElement emailField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"input-:Rb6l6:\"]")));
-            emailField.sendKeys("riaayugmail.com");
+            emailField.sendKeys("markushulu@gmail.com");
             WebElement passwordField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"input-:Rd6l6:\"]")));
-            passwordField.sendKeys("Surikiti"); 
+            passwordField.sendKeys("Sutarno123"); 
             WebElement confirmPasswordField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"input-:Rf6l6:\"]")));
-            confirmPasswordField.sendKeys("Surikiti");
+            confirmPasswordField.sendKeys("Sutarno123");
             WebElement checkbox = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"register__checkbox__tnc\"]")));
             checkbox.click();
 
             WebElement submitButton = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"__next\"]/div/div[2]/div[2]/div/form/button")));
 
-    
-  if (submitButton.isEnabled()) {
-    submitButton.click();
-    wait.until(ExpectedConditions.urlContains("dashboard"));
+            if (submitButton.isEnabled()) {
+                submitButton.click();
+                wait.until(ExpectedConditions.urlContains("dashboard"));
 
-    if (driver.getCurrentUrl().contains("dashboard")) {
-        System.out.println("Registrasi Berhasil Dilakukan");
-    } else {
-        System.out.println("Registrasi Gagal Dilakukan");
+                if (driver.getCurrentUrl().contains("dashboard")) {
+                    System.out.println("Registrasi Berhasil Dilakukan");
+                } else {
+                    System.out.println("Registrasi Gagal Dilakukan");
+                }
+            } else {
+                System.out.println("Submit button is disabled due to invalid input.");
+            }
+
+            boolean isErrorPresent = false;
+            try {
+                WebElement nameErrorMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"__next\"]/div/div[2]/div[2]/div/form/div[1]/div[2]/p")));
+                if (nameErrorMessage.isDisplayed()) {
+                    isErrorPresent = true;
+                }
+            } catch (Exception e) { 
+            }
+
+            try {
+                WebElement phoneErrorMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"__next\"]/div/div[2]/div[2]/div/form/div[2]/div/div[2]/p")));
+                if (phoneErrorMessage.isDisplayed()) {
+                    isErrorPresent = true;
+                }
+            } catch (Exception e) { 
+            }
+
+            try {
+                WebElement businessNameErrorMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"__next\"]/div/div[2]/div[2]/div/form/div[3]/div[2]/p")));
+                if (businessNameErrorMessage.isDisplayed()) {
+                    isErrorPresent = true;
+                    System.out.println("Error in Business Name: " + businessNameErrorMessage.getText());
+                }
+            } catch (Exception e) { 
+            }
+
+            try {
+                WebElement emailErrorMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"__next\"]/div/div[2]/div[2]/div/form/div[5]/div[2]/p")));
+                if (emailErrorMessage.isDisplayed()) {
+                    isErrorPresent = true;
+                }
+            } catch (Exception e) { 
+            }
+
+            try {
+                WebElement passwordErrorMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"__next\"]/div/div[2]/div[2]/div/form/div[6]/div[2]/p")));
+                if (passwordErrorMessage.isDisplayed()) {
+                    isErrorPresent = true;
+                }
+            } catch (Exception e) { 
+            }
+
+            if (!isErrorPresent) {
+                System.out.println("Registrasi Berhasil Dilakukan");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-} else {
-    System.out.println("Submit button is disabled due to invalid input.");
-}
-
-boolean isErrorPresent = false;
-try {
-    WebElement nameErrorMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"__next\"]/div/div[2]/div[2]/div/form/div[1]/div[2]/p")));
-    if (nameErrorMessage.isDisplayed()) {
-        isErrorPresent = true;
-    }
-} catch (Exception e) { 
-}
-
-try {
-    WebElement phoneErrorMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"__next\"]/div/div[2]/div[2]/div/form/div[2]/div/div[2]/p")));
-    if (phoneErrorMessage.isDisplayed()) {
-        isErrorPresent = true;
-    }
-} catch (Exception e) { 
-}
-
-try {
-    WebElement businessNameErrorMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"__next\"]/div/div[2]/div[2]/div/form/div[3]/div[2]/p")));
-    if (businessNameErrorMessage.isDisplayed()) {
-        isErrorPresent = true;
-    }
-} catch (Exception e) { 
-}
-
-try {
-    WebElement emailErrorMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"__next\"]/div/div[2]/div[2]/div/form/div[5]/div[2]/p")));
-    if (emailErrorMessage.isDisplayed()) {
-        isErrorPresent = true;
-    }
-} catch (Exception e) { 
-}
-
-try {
-    WebElement passwordErrorMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"__next\"]/div/div[2]/div[2]/div/form/div[6]/div[2]/p")));
-    if (passwordErrorMessage.isDisplayed()) {
-        isErrorPresent = true;
-    }
-} catch (Exception e) { 
-}
-
-if (!isErrorPresent) {
-    System.out.println("Registrasi Berhasil Dilakukan");
-}
-
-} catch (Exception e) {
-e.printStackTrace();
-}
-}
 }
