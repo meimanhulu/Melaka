19ae3fd4c40c043f9bfec07a85460f8a506455d7
meimanhulu <94421861+meimanhulu@users.noreply.github.com>
2025-01-24 16:07:04 +0700

    push

--- src/main/java/com/testing/Registration.java
+++ src/main/java/com/testing/Registration.java
@@ -1,10 +1,13 @@
 package com.testing;
 
 import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
@@ -13,8 +16,15 @@ public class Registration {
     public static void main(String[] args) {
         System.out.println("Selenium 4");
         System.setProperty("webdriver.chrome.driver", "C:\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+        
+        // // ChromeOptions to prevent crash
+        // ChromeOptions options = new ChromeOptions();
+        // options.addArguments("--no-sandbox");
+        // options.addArguments("--disable-dev-shm-usage");
+        // options.addArguments("--remote-allow-origins=*");
+
         WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         driver.manage().window().maximize();
         driver.get("https://dashboard.melaka.app/register");
 
@@ -31,21 +41,21 @@ public class Registration {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
         try {
             WebElement nameField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"input-:R36l6:\"]")));
-            nameField.sendKeys("Cash"); 
+            nameField.sendKeys("Catetya"); 
             WebElement nameErrorMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"__next\"]/div/div[2]/div[2]/div/form/div[1]/div[2]/p")));
             if (nameErrorMessage.isDisplayed()) {
                 System.out.println("Error in Nama: " + nameErrorMessage.getText());
             }
 
             WebElement phoneField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"input-:R156l6:\"]")));
-            phoneField.sendKeys("8192121");
+            phoneField.sendKeys("81382143466");
             WebElement phoneErrorMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"__next\"]/div/div[2]/div[2]/div/form/div[2]/div/div[2]/p")));
             if (phoneErrorMessage.isDisplayed()) {
                 System.out.println("Error in Phone: " + phoneErrorMessage.getText());
             }
 
             WebElement businessNameField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"input-:R76l6:\"]")));
-            businessNameField.clear(); // Jika nama Bisnis Kosong
+            businessNameField.sendKeys("Toko Abi"); // Jika nama Bisnis Kosong
             WebElement businessNameErrorMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"__next\"]/div/div[2]/div[2]/div/form/div[3]/div[2]/p")));
             if (businessNameErrorMessage.isDisplayed()) {
                 System.out.println("Error in Business Name: " + businessNameErrorMessage.getText()); //maka hasilnya ditampilkan disini
@@ -55,21 +65,21 @@ public class Registration {
             businessTypeField.click();
 
             WebElement emailField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"input-:Rb6l6:\"]")));
-            emailField.sendKeys("kuculgmail.com");
+            emailField.sendKeys("hulu@gmail.com");
             WebElement emailErrorMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"__next\"]/div/div[2]/div[2]/div/form/div[5]/div[2]/p")));
             if (emailErrorMessage.isDisplayed()) {
                 System.out.println("Error in Email: " + emailErrorMessage.getText());
             }
 
             WebElement passwordField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"input-:Rd6l6:\"]")));
-            passwordField.sendKeys("kami1");
+            passwordField.sendKeys("tami1234");
             WebElement passwordErrorMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"__next\"]/div/div[2]/div[2]/div/form/div[6]/div[2]/p")));
             if (passwordErrorMessage.isDisplayed()) {
                 System.out.println("Error in Password: " + passwordErrorMessage.getText());
             }
 
             WebElement confirmPasswordField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"input-:Rf6l6:\"]")));
-            confirmPasswordField.sendKeys("kami23");
+            confirmPasswordField.sendKeys("tami1234");
             WebElement confirmPasswordErrorMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"__next\"]/div/div[2]/div[2]/div/form/div[7]/div[2]/p")));
             if (confirmPasswordErrorMessage.isDisplayed()) {
                 System.out.println("Error in Confirm Password: " + confirmPasswordErrorMessage.getText());
