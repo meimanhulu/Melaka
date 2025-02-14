bf9be83fd761de57ef8d8cb478cb4b2eddaecddb
meimanhulu <94421861+meimanhulu@users.noreply.github.com>
2025-02-14 20:01:41 +0700

    push

--- src/main/java/com/testing/Registration.java
+++ src/main/java/com/testing/Registration.java
@@ -1,7 +1,9 @@
 package com.testing;
 
 import java.time.Duration;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -14,7 +16,7 @@ public class Registration {
     public static void main(String[] args) {
         System.out.println("Selenium 4");
         System.setProperty("webdriver.chrome.driver", "C:\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
-       WebDriver driver = new ChromeDriver();
+        WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         driver.manage().window().maximize();
         driver.get("https://dashboard.melaka.app/register");
@@ -30,35 +32,48 @@ public class Registration {
 
     public static void testRegistrationProcess(WebDriver driver) {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        Random random = new Random();
+        long min = 8139L; // Gunakan long untuk nilai yang besar
+        long max = 81391199999L;
+        long randomPhoneNumber = min + (long) (random.nextDouble() * (max - min + 1));
+    
         try {
-
-            WebElement nameField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"input-:R36l6:\"]")));
-            nameField.sendKeys("Sahacuy");
-
-            WebElement phoneField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"input-:R156l6:\"]")));
-            phoneField.sendKeys("81391103602");
-
-            WebElement businessNameField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"input-:R76l6:\"]")));
-            businessNameField.sendKeys("Yellow Store");
-
-            WebElement businessTypeField = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"online_seller-:Ri96l6:\"]")));
+            // Menggunakan By.id
+            WebElement nameField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("input-name")));
+            nameField.sendKeys("Sahswa");
+    
+            // Menggunakan By.name
+            WebElement phoneField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("phone")));
+            phoneField.sendKeys(String.valueOf(randomPhoneNumber));
+    
+            // Menggunakan By.cssSelector
+            WebElement businessNameField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input#business-name")));
+            businessNameField.sendKeys("blue struck");
+    
+            // Menggunakan By.className
+            WebElement businessTypeField = wait.until(ExpectedConditions.elementToBeClickable(By.className("business-type")));
             businessTypeField.click();
-
-            WebElement emailField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"input-:Rb6l6:\"]")));
-            emailField.sendKeys("markus29@gmail.com");
-
-            WebElement passwordField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"input-:Rd6l6:\"]")));
+    
+            // Menggunakan By.tagName
+            WebElement emailField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.tagName("input")));
+            emailField.sendKeys("marcus81@gmail.com");
+    
+            // Menggunakan By.cssSelector dengan atribut
+            WebElement passwordField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[type='password']")));
             passwordField.sendKeys("Valid123#");
-
-            WebElement confirmPasswordField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"input-:Rf6l6:\"]")));
+    
+            // Menggunakan By.xpath relatif
+            WebElement confirmPasswordField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@name='confirmPassword']")));
             confirmPasswordField.sendKeys("Valid123#");
-
-            WebElement checkbox = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"register__checkbox__tnc\"]")));
+    
+            // Menggunakan By.cssSelector
+            WebElement checkbox = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("input[type='checkbox']")));
             if (!checkbox.isSelected()) {
                 checkbox.click();
             }
-
-            WebElement submitButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"__next\"]/div/div[2]/div[2]/div/form/button")));
+    
+            // Menggunakan By.cssSelector
+            WebElement submitButton = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button[type='submit']")));
             if (submitButton.isEnabled()) {
                 submitButton.click();
                 wait.until(ExpectedConditions.urlContains("dashboard"));
@@ -70,7 +85,7 @@ public class Registration {
             } else {
                 System.out.println("Tombol Daftar tidak aktif karena input tidak valid.");
             }
-
+    
         } catch (Exception e) {
             e.printStackTrace();
         }
